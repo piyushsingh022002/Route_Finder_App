d96f9df28761bf5748563f3dae71eda0dd2ad15e
Piyush Singh <b221037@iiit-bh.ac.in>
2025-05-29 17:05:21 +0530

    Removed Mapbox dependencies, switched to OpenStreetMap with Leaflet

---
+++ frontend_Web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import MapView from './components/MapView';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Route Customization Booking App</h1>
+      <MapView />
+    </div>
+  );
+}
+
+export default App;
